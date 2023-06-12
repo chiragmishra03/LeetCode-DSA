c62e6c3bba6b5ae62ffc239b9ec20eeddeb865d5
CHIRAG MISHRA <92207681+chiragmishra03@users.noreply.github.com>
2023-06-12 19:09:06 +0530

    Time: 9 ms (22.20%), Space: 15.8 MB (18.27%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     
-void help(vector<string>&ans ,  int open, int close, string&in) {
+void help(vector<string>&ans ,  int open, int close, string in) {
 	if (open == 0 && close == 0) {
 		ans.push_back(in);
 		return;
 	}
 	if (open > 0) {
-		in.push_back('(');
-		help(ans,open - 1, close, in);
-		in.pop_back();
-
+		help(ans,open - 1, close, in+='(');
+        in.pop_back();
+        
 	}
 
 	if (close > 0 && open < close) {
-		in.push_back(')');
-		help(ans,open, close - 1, in);
-		in.pop_back();
+		help(ans,open, close - 1, in+=')');
+        in.pop_back();
 	}
 	return;
 }
