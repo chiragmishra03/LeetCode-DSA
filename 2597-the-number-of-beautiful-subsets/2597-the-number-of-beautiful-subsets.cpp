513cee1e54b00928a44b6d22cd9f4954130e6719
CHIRAG MISHRA <92207681+chiragmishra03@users.noreply.github.com>
2023-06-12 19:57:08 +0530

    Time: 1119 ms (31.84%), Space: 36.6 MB (23.70%) - LeetHub

--- 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
@@ -1,27 +1,25 @@
 class Solution {
-public:
-    
-int help(int i, vector<int>& nums, int k, unordered_map<int, int>& mp) {
-	if (i == nums.size()) { 
-		if(!mp.empty()){
-            return 1;
-        }
-        return 0;
+public: 
+int help(int i, vector<int>& nums, int k, unordered_map<int,int> &mp) {
+	if (i == nums.size())
+	{
+		return 1;
 	}
 	int taken = 0;
-	if (!mp[nums[i] - k] && !mp[nums[i]+k])
-	{
-		mp[nums[i]]++;
-		taken = help(i + 1, nums, k, mp);
-		mp[nums[i]]--;
-	}
-	int not_taken = help(i + 1, nums, k, mp);
+	if (mp[nums[i] - k] == 0 && mp[nums[i] + k] == 0)
+        {
+			mp[nums[i]]++;
+			taken =  help(i + 1, nums, k, mp);
+			mp[nums[i]]--;
+		}
+	int not_taken = 0;
+    not_taken = help(i + 1, nums, k, mp);
 	return taken + not_taken;
 }
 int beautifulSubsets(vector<int>& nums, int k) {
-       unordered_map<int, int> mp;
-       sort(nums.begin(),nums.end());
-       return help(0,nums,k,mp)-1;
+    unordered_map<int,int> mp;
+    sort(nums.begin(),nums.end());
+    return help(0,nums,k,mp)-1;
         
-    }
+}
 };
