6501ec1e0948f8b134980c9e6d6a4e818ef63dc8
CHIRAG MISHRA <92207681+chiragmishra03@users.noreply.github.com>
2023-06-15 23:10:10 +0530

    Time: 12 ms (72.03%), Space: 8.5 MB (52.71%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        priority_queue<pair<int,char>> pq;
+        unordered_map<char,int> mp;
+        for(int i=0;i<s.size();i++)
+            mp[s[i]]++;
+        for(auto i:mp)
+            pq.push(make_pair(i.second,i.first));
+        string ans="";
+        while(!pq.empty())
+        {
+            int a=pq.top().first;
+            char b= pq.top().second;
+            for(int i=0;i<a;i++)
+            {
+                ans+=b;
+            }
+            pq.pop();
+        }
+        return ans;
+        
+        
+        
+        
+    }
+};
