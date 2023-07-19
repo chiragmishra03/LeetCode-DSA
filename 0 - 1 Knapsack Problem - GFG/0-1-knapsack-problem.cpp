4cbc6c251edbcab567bcd9a3a89bb1fe94f3f876
CHIRAG MISHRA <92207681+chiragmishra03@users.noreply.github.com>
2023-07-19 16:13:24 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -7,37 +7,57 @@ using namespace std;
 class Solution
 {
     public:
-    int dp[1001][1001];
-    int solve(int W, int wt[], int val[], int n,int i)
-    { 
+    // int solve(int W, int wt[], int val[], int n,int i)
+    // { 
         
-        if(W==0 || i<0){
-            return 0;
-        }
-        if( dp[i][W] !=-1){
-            return  dp[i][W] ;
-        }
-        int ans1;
-        int ans2;
-        int ans3;
-        if(W>=wt[i]){
-            //include
-           ans1 = val[i]+solve(W-wt[i] , wt, val,n,i-1);
-           //exclude
-           ans2 = solve(W , wt, val , n , i-1);
-           return dp[i][W] = max(ans1 , ans2);
+    //     if(W==0 || i<0){
+    //         return 0;
+    //     }
+    //     if( dp[i][W] !=-1){
+    //         return  dp[i][W] ;
+    //     }
+    //     int ans1;
+    //     int ans2;
+    //     int ans3;
+    //     if(W>=wt[i]){
+    //         //include
+    //       ans1 = val[i]+solve(W-wt[i] , wt, val,n,i-1);
+    //       //exclude
+    //       ans2 = solve(W , wt, val , n , i-1);
+    //       return dp[i][W] = max(ans1 , ans2);
             
-        }
-        else{
-            ans3 = solve(W , wt, val , n , i-1);
-            return  dp[i][W] =  ans3;
-        }
-    }
+    //     }
+    //     else{
+    //         ans3 = solve(W , wt, val , n , i-1);
+    //         return  dp[i][W] =  ans3;
+    //     }
+    // }
+    
+    int bottomup(int W, int wt[], int val[], int n) {
+	int dp[1001][1001];
+	memset(dp , 0 , sizeof dp);
+
+	int ans1 , ans2, ans3;
+	for (int i = 1; i < n + 1; i++) {
+		for (int j = 1; j < W + 1; j++) {
+			if (j >= wt[i - 1]) {
+				dp[i][j] = max(val[i - 1] + dp[i - 1][j - wt[i - 1]], dp[i - 1][j]);
+			}
+			else {
+				dp[i][j] =  dp[i - 1][j];
+			}
+
+		}
+	}
+
+	return dp[n][W];
+
+
+}
     //Function to return max value that can be put in knapsack of capacity W.
     int knapSack(int W, int wt[], int val[], int n) 
     { 
-        memset(dp, -1, sizeof dp);
-        return solve(W , wt,val,n,n-1);
+        return bottomup(W , wt,val,n);
     }
 };
 
