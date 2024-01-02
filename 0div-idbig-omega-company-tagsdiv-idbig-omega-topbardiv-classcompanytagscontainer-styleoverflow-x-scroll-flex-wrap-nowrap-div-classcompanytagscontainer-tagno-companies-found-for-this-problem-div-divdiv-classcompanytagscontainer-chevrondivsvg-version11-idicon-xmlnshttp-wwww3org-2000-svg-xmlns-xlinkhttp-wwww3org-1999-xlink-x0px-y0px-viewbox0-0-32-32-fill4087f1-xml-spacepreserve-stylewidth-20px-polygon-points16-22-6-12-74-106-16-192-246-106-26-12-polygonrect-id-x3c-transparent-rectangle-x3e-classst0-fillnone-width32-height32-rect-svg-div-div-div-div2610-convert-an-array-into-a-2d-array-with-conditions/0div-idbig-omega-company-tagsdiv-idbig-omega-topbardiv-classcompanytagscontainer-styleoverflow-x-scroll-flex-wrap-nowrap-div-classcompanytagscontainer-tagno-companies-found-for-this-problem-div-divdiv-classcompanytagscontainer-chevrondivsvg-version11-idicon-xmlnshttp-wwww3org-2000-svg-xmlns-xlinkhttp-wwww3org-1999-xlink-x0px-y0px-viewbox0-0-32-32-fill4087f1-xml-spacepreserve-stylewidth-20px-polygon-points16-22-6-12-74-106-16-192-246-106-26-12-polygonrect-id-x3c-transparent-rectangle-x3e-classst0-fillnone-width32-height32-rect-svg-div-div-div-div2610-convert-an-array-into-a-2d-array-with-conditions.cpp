40c515e46cdd215f2b58353e4dbf38ae7378a833
CHIRAG MISHRA <92207681+chiragmishra03@users.noreply.github.com>
2024-01-02 12:11:21 +0530

    Time: 28 ms (5.35%), Space: 30 MB (70.96%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2610-convert-an-array-into-a-2d-array-with-conditions/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2610-convert-an-array-into-a-2d-array-with-conditions.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2610-convert-an-array-into-a-2d-array-with-conditions/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2610-convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -1,25 +1,15 @@
 class Solution {
 public:
     vector<vector<int>> findMatrix(vector<int>& nums) {
-    unordered_map<int, int> mp;
-    for (int num : nums) {
-        mp[num]++;
-    }
-    vector<vector<int>> ans;
-    while (!mp.empty()) {
-        vector<int> v;
-        for (auto it = mp.begin(); it != mp.end();) {
-            v.push_back(it->first);
-            it->second--;
-            if (it->second == 0) {
-                it = mp.erase(it);
-            } else {
-                ++it;
+    vector<int> freq(nums.size() + 1);
+        vector<vector<int>>ans;
+        for(int i=0;i<nums.size();i++){
+            if(freq[nums[i]]>=ans.size()){
+                ans.push_back({});
             }
+            ans[freq[nums[i]]].push_back(nums[i]);
+            freq[nums[i]]++;
         }
-        ans.push_back(v);
-        
-    }
         return ans;
     }
 };
