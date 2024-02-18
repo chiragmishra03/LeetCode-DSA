d75f42aaf3b5cb020a79be45207639ed27e6110f
CHIRAG MISHRA <92207681+chiragmishra03@users.noreply.github.com>
2024-02-18 12:10:23 +0530

    Time: 4 ms (12.22%), Space: 8.4 MB (9.71%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<string>ans;
+    vector<string>mapping = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    void solve(string digits, string cur, int i)
+{
+	if (i == digits.size()) {
+        if(cur.size()>0)
+		ans.push_back(cur);
+        return;
+	}
+	int num = digits[i] - '0';
+	string s = mapping[num];
+	for (int idx = 0 ; idx < s.length(); idx++) {
+		cur.push_back(s[idx]);
+		solve(digits, cur , i + 1);
+		cur.pop_back();
+	}
+}
+    
+    
+    vector<string> letterCombinations(string digits) {
+    string cur;
+	solve(digits, cur, 0);
+        return ans;
+    }
+};
