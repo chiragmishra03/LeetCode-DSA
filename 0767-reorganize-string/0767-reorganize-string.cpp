8c79a4d34a49fe946d214631693433561107f173
CHIRAG MISHRA <92207681+chiragmishra03@users.noreply.github.com>
2024-02-20 16:02:32 +0530

    Time: 2 ms (56.49%), Space: 8.3 MB (22.97%) - LeetHub

---
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    string reorganizeString(string s) {
+        priority_queue<pair<int,char>>pq;
+        vector<int>freq(26,0);
+        for(int i=0;i<s.size();i++)freq[s[i]-'a']++;
+        for(int i=0;i<26;i++){
+            char c = i+'a';
+            if(freq[i]!=0){
+                pair<int,char>p = make_pair(freq[i],c);
+                pq.push(p);
+            }
+        }
+        string ans="";
+        while(!pq.empty()){
+            if(pq.size()>=2){
+                pair<int,char>p1;
+                pair<int,char>p2;
+                p1=pq.top();
+                pq.pop();
+                p2 = pq.top();
+                pq.pop();
+                ans+=p1.second;
+                ans+=p2.second;
+                p1.first--;
+                p2.first--;
+                if(p1.first>0)pq.push(p1);
+                if(p2.first>0)pq.push(p2);
+            }
+            else {
+                if (pq.top().first > 1 || (!ans.empty() && ans[ans.size() - 1] == pq.top().second))
+return "";
+                else{
+                    ans+=pq.top().second;
+                    pq.pop();
+                }
+            }
+        }
+        
+        return ans;
+        
+        
+        
+        
+    }
+};
